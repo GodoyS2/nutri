1ef0d2507dca8cad22b805a70ab4c92ae2964898
Gilberto Godoy <88116025+GodoyS2@users.noreply.github.com>
2025-06-03 15:42:51 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-import Testimonials from './components/Testimonials';
+// import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
 function App() {
